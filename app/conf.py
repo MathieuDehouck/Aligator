d3f6817d0406348e7dab0603664c75ed62fd1b8d
Mathieu Dehouck <email>
2024-03-07 15:58:35 +0100

    update de conf.py pour prendre en compte la nouvelle arborescence

--- app/conf.py
+++ app/conf.py
@@ -12,11 +12,11 @@ autodoc_class_signature = "separated"
 
 def setup(app):
 
-    #path.append('app/core/')
+    path.append('../../')
     
     #app.add_css_file('custom.css')  # may also be an URL
     try:
-        copy('app/custom.css', 'doc/_static/custom.css')
+        copy('../../app/custom.css', '../../doc/_static/custom.css')
     except:
         ()
     #copy('custom.css', '../doc/_static/custom.css')
